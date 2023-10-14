f2953279bf03905e741a43fcbeec545274c0a436
Johny270 <jeantakam56@yahoo.com>
2023-10-14 16:07:11 -0300

    Create AllClubs directory, stub up AllClub component

---
+++ src/components/AllClubs/AllClubs.jsx
@@ -0,0 +1,11 @@
+const AllClubs = () => {
+  return ( 
+    <div className="clubs-container">
+      <div className="club">
+        <h1>ALL SHOPS</h1>
+      </div>
+    </div>
+  )
+}
+ 
+export default AllClubs;
