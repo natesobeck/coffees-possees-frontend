0ec98d4609081c6bd33bfa3c897ac51f24ff4120
Nate Sobeck <nsobeck910@gmail.com>
2023-10-14 17:51:56 -0400

    added draft landing page greeting

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -11,7 +11,15 @@ const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <section className={styles['section-container']}>
-      <h1>Welcome to Posses and Coffees, {user ? user.name.slice(0,1).toUpperCase().concat(user.name.slice(1).toLowerCase()) : 'friend'}</h1>
+      <h1>Welcome, {user ? user.name.slice(0,1).toUpperCase().concat(user.name.slice(1).toLowerCase()) : 'friend'}</h1>
+      <img src="/Logo.png" alt="Coffees and Posses Logo" />
+      <br />
+      <p>Do you love coffee?</p>
+      <br />
+      <p>Got some interests but don't have anybody to talk about them with? Make a club for it!</p>
+      <br />
+      <p>We'd love to join you too. That's what Coffees and Posses is for. Movies, Sports, Dancing, JetSkiing, you name it. There's a club for it.</p>
+      <h2>Bringing Like-Minded People Together</h2>
       </section>
       <section className={styles['section-container']}>
         <h1>This is where the club reccomendations go.</h1>
