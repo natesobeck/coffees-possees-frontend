a32b930155513dfd6eef92dc80bfdd04c83b53db
Nate Sobeck <nsobeck910@gmail.com>
2023-10-14 13:34:33 -0400

    refine landing page layout

--- src/App.jsx
+++ src/App.jsx
@@ -34,9 +34,9 @@ function App() {
   }
 
   return (
-    <>
+    <main>
       <NavBar user={user} handleLogout={handleLogout} />
-      <Routes>
+      <Routes className='main-page'>
         <Route path="/" element={<Landing user={user} />} />
         <Route
           path="/profiles"
@@ -63,7 +63,7 @@ function App() {
           }
         />
       </Routes>
-    </>
+    </main>
   )
 }
 
