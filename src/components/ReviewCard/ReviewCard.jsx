fa2c553f07e62dffbda3c452fb58b80501187f5b
Ellie <eagyeman1234@gmail.com>
2023-10-15 10:12:17 -0400

    add to review card

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -1,10 +1,16 @@
 import styles from './ReviewCard.module.css'
 
 const ReviewCard = () => {
-
+  
+ 
   return (
-    <h1>this is a review card</h1>
-    
+    <Link to={}>
+      <header>
+          <span>
+            <h1>this is a review card</h1>
+          </span>
+        </header>
+   </Link>
   )
 }
 
