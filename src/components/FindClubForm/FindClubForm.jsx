9baa1906a673f1d64b3b8ce6e98f354f0d196cfb
Johny270 <jeantakam56@yahoo.com>
2023-10-15 08:22:41 -0300

    Apply more styling to FindClubForm component

--- src/components/FindClubForm/FindClubForm.jsx
+++ src/components/FindClubForm/FindClubForm.jsx
@@ -4,7 +4,7 @@ const FindClubForm = () => {
   return ( 
     <div className="form-container">
       <h1>Search For A Club</h1>
-      <p>Find what clubs are available around your area based on your interests</p>
+      <p className='p-tag'>Find what clubs are available around your area based on your interests</p>
       <form className='search-clubs'>
         <input type="text" placeholder="Enter your city..." className="city-input" />
         <button>See Clubs</button>
