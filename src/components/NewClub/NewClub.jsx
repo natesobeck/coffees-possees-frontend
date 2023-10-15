d43d78fc099af05765c78707b4384b95b310694b
Johny270 <jeantakam56@yahoo.com>
2023-10-15 18:07:44 -0300

    Add clubFormData state in NewClub component, create handleSubmit and handleChange functions as well

--- src/components/NewClub/NewClub.jsx
+++ src/components/NewClub/NewClub.jsx
@@ -1,16 +1,34 @@
 import * as data from '../../../public/assets/data.js'
+import { useState } from 'react'
 import './NewClub.css'
 
-const NewClub = () => {
+const NewClub = (props) => {
+
+  const [clubFormData, setClubFormData] = useState({
+    name: '',
+    category: '',
+    timeOfDay: 'Morning',
+    location: '',
+  })
+
+  const handleChange = evt => {
+    setClubFormData({ ...clubFormData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    props.handleAddClub(clubFormData)
+  }
+
   return (
-    <form className='new-club-container'>
+    <form onSubmit={handleSubmit} className='new-club-container'>
       <div className='club-info'>
         <div className='location-info'>
           <h3>Hello, User!</h3>
           <h5>Let's Create your Club!</h5>
           <p>What is your Club name ?</p>
           <div className='name'>
-            <input type="text" placeholder='E.g: Bookers - A true love for books  ' />
+            <input name="name" type="text" value={clubFormData.name} onChange={handleChange} placeholder='E.g: Bookers - A true love for books  ' />
             {/* <button>Search</button> */}
           </div>
         </div>
@@ -18,11 +36,11 @@ const NewClub = () => {
           <h6>Tell us a bit about the focus of your group... anything useful for other members to know!</h6>
           <div className='category-container'>
             <label htmlFor="category">Category:</label>
-            <input type="text" placeholder='e.g: soccer' />
+            <input name="category" type="text" value={clubFormData.category} onChange={handleChange} placeholder='e.g: soccer' />
           </div>
           <div className='location-time'>
-            <input type="text" placeholder='Enter your city here' />
-            <select name="timeOfDay" id="timeOfDay">
+            <input name="location" type="text" value={clubFormData.location} placeholder='Enter your city here' />
+            <select name="timeOfDay" value={clubFormData.timeOfDay} onChange={handleChange} id="timeOfDay">
               <option value="morning">Morning</option>
               <option value="afternoon">Afternoon</option>
               <option value="Evening">Evening</option>
@@ -31,7 +49,7 @@ const NewClub = () => {
         </div>
       </div>
       <div className='create-club-button'>
-        <button>Create Club</button>
+        <button type='submit'>Create Club</button>
       </div>
       
       <div className='shops-in-club'>
@@ -41,11 +59,11 @@ const NewClub = () => {
           <div>Shop Card 2</div>
           <div>Shop Card 3</div>
         </div>
-        <form className='time-slot'>
+        <div className='time-slot'>
           <label>Pick a time:</label>
           <input type="time" placeholder="Pick a time slot" />
           <button>Pick Time</button>
-        </form>
+        </div>
       </div>
     </form>
   )
