9d32b740b825afa8294cb753df27d693455da37f
Johny270 <jeantakam56@yahoo.com>
2023-10-15 10:13:34 -0300

    Add styling to NewClub component

--- src/components/NewClub/NewClub.jsx
+++ src/components/NewClub/NewClub.jsx
@@ -2,32 +2,37 @@ import * as data from '../../../public/assets/data.js'
 import './NewClub.css'
 
 const NewClub = () => {
-  return ( 
-    <main>
-      <div>
+  return (
+    <main className='new-club-container'>
+      <div className='club-info'>
         <div className='location-info'>
           <h3>Hello, User!</h3>
           <h5>Where do you want to start your club?</h5>
           <p>Enter a Zip code or City:</p>
           <form>
-            <input type="text" />
+            <input type="text" placeholder='zipcode or city' />
             <button>Submit</button>
           </form>
         </div>
-        <div className='group-focus'>
-          <p>Tell us a bit about the focus of your group, anything useful for other members to know!</p>
-          <label htmlFor="category">Category</label>
-          <option value="category" id='category'>
-            <select name="category" id="category-option">Option1</select>
-            <select name="category" id="category-option">Option2</select>
-            <select name="category" id="category-option">Option3</select>
-            <select name="category" id="category-option">Option4</select>
-          </option>
-          <input type="text" placeholder='Specific focus' />
-          <input type="number" placeholder='Age Group' />
+        <div className='club-focus'>
+          <h6>Tell us a bit about the focus of your group, anything useful for other members to know!</h6>
+          <div className='category-container'>
+            <label htmlFor="category">Category:</label>
+            <select value="category" id='category'>
+              <option name="category" id="category-option">Option1</option>
+              <option name="category" id="category-option">Option2</option>
+              <option name="category" id="category-option">Option3</option>
+              <option name="category" id="category-option">Option4</option>
+            </select>
+          </div>
+          <form>
+            <input type="text" placeholder='Specific focus' />
+            <input type="number" placeholder='Age Group' />
+          </form>
         </div>
         <div></div>
       </div>
+      <div className='shops-on-new-club'></div>
     </main>
   )
 }
