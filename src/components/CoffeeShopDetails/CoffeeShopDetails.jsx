0d8c91ddc874859dcc719e83c6ceb9cf507e5fa8
Ellie <eagyeman1234@gmail.com>
2023-10-16 10:32:39 -0400

    add state, uesEffect functions for coffeeshopdetails

--- src/components/CoffeeShopDetails/CoffeeShopDetails.jsx
+++ src/components/CoffeeShopDetails/CoffeeShopDetails.jsx
@@ -1,12 +1,46 @@
+//css
 import styles from './CoffeeShopDetails.module.css'
+
+//services
+import * as shopService from '../../services/shopService'
+
+//npm modules
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+
+//components
+
+
+
 
 //import loading, new review, and reviews here 
 
 const CoffeeShopDetails = () => {
+  const [coffeeShop, setCoffeeShop] = useState({})
+  const { coffeeShopId } = useParams()
+
+  useEffect(() => {
+    const fetchCoffeeShop = async () => {
+      const data = await shopService.show(coffeeShopId)
+      setCoffeeShop(data)
+    }
+    fetchCoffeeShop()
+  }, [coffeeShopId])
+
+
+
 
   return (
-    <main>
+    <main className={styles.container}>
+      <div className={styles['coffeeShop-details-container']}>
+
+      </div>
       
+
+
+
+
 
 
     </main>
@@ -15,6 +49,12 @@ const CoffeeShopDetails = () => {
 }
 
 export default CoffeeShopDetails
+
+
+
+
+
+
 
 
 //refer to hoot details
