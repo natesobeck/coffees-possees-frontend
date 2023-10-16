20f864bf0d869363604874090ec440da5c182c90
Johny270 <jeantakam56@yahoo.com>
2023-10-16 09:18:57 -0300

    Define state for clubs and create function to handle club creation in App component

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,14 @@ import ClubDetails from './components/ClubDetails/ClubDetails'
 
 // services
 import * as authService from './services/authService'
+import * as clubService from './services/clubService'
 
 // styles
 import './App.css'
 
 function App() {
   const [user, setUser] = useState(authService.getUser())
+  const [clubs, setClubs] = useState([])
   const navigate = useNavigate()
 
   const handleLogout = () => {
@@ -35,6 +37,12 @@ function App() {
 
   const handleAuthEvt = () => {
     setUser(authService.getUser())
+  }
+
+  const handleAddClub = async clubFormData => {
+    const newClub = await clubService.create(clubFormData)
+    setClubs([newClub, ...clubs])
+    navigate('/clubs')
   }
 
   return (
