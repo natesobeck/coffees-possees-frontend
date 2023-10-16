529e5ba3a55cb2582dcc5d95d430ace5d35863d8
Ellie <eagyeman1234@gmail.com>
2023-10-15 21:01:25 -0400

    add to editcoffeeshop

--- src/components/EditCoffeeShop/EditCoffeeShop.jsx
+++ src/components/EditCoffeeShop/EditCoffeeShop.jsx
@@ -1,7 +1,8 @@
 import styles from './EditCoffeeShop.module.css'
 
 const EditCoffeeShop = () => {
-  <h1> edit coffee shop is the same as new coffee shop with minor changes</h1>
+  <h1> edit coffee shop is the same as new coffee shop with minor changes once this is done</h1>
 }
 
 export default EditCoffeeShop
+
