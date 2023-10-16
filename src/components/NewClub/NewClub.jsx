f11eccc16f37e197a5817793ec8839109cbafcea
Johny270 <jeantakam56@yahoo.com>
2023-10-16 09:41:39 -0300

    Correct missing onChange prop on input field and complete create club functionality

--- src/components/NewClub/NewClub.jsx
+++ src/components/NewClub/NewClub.jsx
@@ -21,51 +21,53 @@ const NewClub = (props) => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className='new-club-container'>
-      <div className='club-info'>
-        <div className='location-info'>
-          <h3>Let's Create Your New Club!</h3>
-          <h5>Start by giving your club a stunning name that will grab interest!</h5>
-          <p>What will yor club be called ?</p>
-          <div className='name'>
-            <input name="name" type="text" value={clubFormData.name} onChange={handleChange} placeholder='E.g: Bookers - A true love for books  ' />
-            {/* <button>Search</button> */}
+    <div className='new-club-container'>
+      <form onSubmit={handleSubmit} >
+        <div className='club-info'>
+          <div className='location-info'>
+            <h3>Let's Create Your New Club!</h3>
+            <h5>Start by giving your club a stunning name that will grab interest!</h5>
+            <p>What will yor club be called ?</p>
+            <div className='name'>
+              <input name="name" type="text" value={clubFormData.name} onChange={handleChange} placeholder='E.g: Bookers - A true love for books  ' />
+              {/* <button>Search</button> */}
+            </div>
+          </div>
+          <div className='club-focus'>
+            <h6>Tell us a bit about your group... Provide to other members information about its focus, location and the like.</h6>
+            <div className='category-container'>
+              <label htmlFor="category">Category:</label>
+              <input name="category" type="text" value={clubFormData.category} onChange={handleChange} placeholder='e.g: soccer' />
+            </div>
+            <div className='location-time'>
+              <input name="location" type="text" value={clubFormData.location} onChange={handleChange} placeholder='Enter your city here' />
+              <select name="timeOfDay" value={clubFormData.timeOfDay} onChange={handleChange} id="timeOfDay">
+                <option value="Morning">Morning</option>
+                <option value="AfterNoon">Afternoon</option>
+                <option value="Evening">Evening</option>
+              </select>
+            </div>
           </div>
         </div>
-        <div className='club-focus'>
-          <h6>Tell us a bit about your group... Provide to other members information about its focus, location and the like.</h6>
-          <div className='category-container'>
-            <label htmlFor="category">Category:</label>
-            <input name="category" type="text" value={clubFormData.category} onChange={handleChange} placeholder='e.g: soccer' />
+        <div className='create-club-button'>
+          <button type='submit'>Create Club</button>
+        </div>
+      </form>
+        <div className='shops-in-club'>
+          <p>Here's the coffee shops in you area! Select a shop to host your club in and let others join!</p>
+          <div className='shop-cards'>
+            <div>Shop Card 1</div>
+            <div>Shop Card 2</div>
+            <div>Shop Card 3</div>
           </div>
-          <div className='location-time'>
-            <input name="location" type="text" value={clubFormData.location} placeholder='Enter your city here' />
-            <select name="timeOfDay" value={clubFormData.timeOfDay} onChange={handleChange} id="timeOfDay">
-              <option value="morning">Morning</option>
-              <option value="afternoon">Afternoon</option>
-              <option value="Evening">Evening</option>
-            </select>
-          </div>
+          {/* <div className='time-slot'>
+            <label>Pick a time:</label>
+            <input type="time" placeholder="Pick a time slot" />
+            <button>Pick Time</button>
+          </div> */}
         </div>
-      </div>
-      <div className='create-club-button'>
-        <button type='submit'>Create Club</button>
-      </div>
-      
-      <div className='shops-in-club'>
-        <p>Here's the coffee shops in you area! Select a shop to host your club in and let others join!</p>
-        <div className='shop-cards'>
-          <div>Shop Card 1</div>
-          <div>Shop Card 2</div>
-          <div>Shop Card 3</div>
-        </div>
-        <div className='time-slot'>
-          <label>Pick a time:</label>
-          <input type="time" placeholder="Pick a time slot" />
-          <button>Pick Time</button>
-        </div>
-      </div>
-    </form>
+    </div>
+    
   )
 }
 
