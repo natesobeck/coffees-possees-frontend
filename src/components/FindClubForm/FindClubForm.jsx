ebefa01b40c8871fb629625d2d05cc61d28d4e99
Nate Sobeck <nsobeck910@gmail.com>
2023-10-16 17:40:28 -0400

    switch all css to modules

--- src/components/FindClubForm/FindClubForm.jsx
+++ src/components/FindClubForm/FindClubForm.jsx
@@ -1,12 +1,12 @@
-import './FindClubForm.css'
+import styles from './FindClubForm.module.css'
 
 const FindClubForm = () => {
   return ( 
-    <div className="form-container">
+    <div className={styles["form-container"]}>
       <h1>Search For A Club</h1>
-      <p className='p-tag'>Find what clubs are available around your area based on your interests</p>
-      <form className='search-clubs'>
-        <input type="text" placeholder="Enter your city..." className="city-input" />
+      <p className={styles['p-tag']}>Find what clubs are available around your area based on your interests</p>
+      <form className={styles['search-clubs']}>
+        <input type="text" placeholder="Enter your city..." className={styles["city-input"]} />
         <button>See Clubs</button>
       </form>
     </div>
--- src/components/NewClub/NewClub.jsx
+++ src/components/NewClub/NewClub.jsx
@@ -1,7 +1,11 @@
-
+// npm modules
 import { useState } from 'react'
-import './NewClub.css'
+
+//components
 import NewCoffeeShop from '../NewCoffeeShop/NewCoffeeShop'
+
+// css
+import styles from './NewClub.module.css'
 
 const NewClub = (props) => {
 
@@ -22,25 +26,25 @@ const NewClub = (props) => {
   }
 
   return (
-    <div className='new-club-container'>
+    <div className={styles['new-club-container']}>
       <form onSubmit={handleSubmit} >
-        <div className='club-info'>
-          <div className='location-info'>
+        <div className={styles['club-info']}>
+          <div className={styles['location-info']}>
             <h3>Let's Create Your New Club!</h3>
             <h5>Start by giving your club a stunning name that will grab interest!</h5>
             <p>What will yor club be called ?</p>
-            <div className='name'>
+            <div className={styles['name']}>
               <input name="name" type="text" value={clubFormData.name} onChange={handleChange} placeholder='E.g: Bookers - A true love for books  ' />
               {/* <button>Search</button> */}
             </div>
           </div>
-          <div className='club-focus'>
+          <div className={styles['club-focus']}>
             <h6>Tell us a bit about your group... Provide to other members information about its focus, location and the like.</h6>
-            <div className='category-container'>
+            <div className={styles['category-container']}>
               <label htmlFor="category">Category:</label>
               <input name="category" type="text" value={clubFormData.category} onChange={handleChange} placeholder='e.g: soccer' />
             </div>
-            <div className='location-time'>
+            <div className={styles['location-time']}>
               <input name="location" type="text" value={clubFormData.location} onChange={handleChange} placeholder='Enter your city here' />
               <select name="timeOfDay" value={clubFormData.timeOfDay} onChange={handleChange} id="timeOfDay" placeholder='Choose time of day'>
                 {/* <option value="" selected disabled>Choose Time of Day</option> */}
@@ -51,14 +55,14 @@ const NewClub = (props) => {
             </div>
           </div>
         </div>
-        <div className='create-club-button'>
+        <div className={styles['create-club-button']}>
           <button type='submit'>Create Club</button>
         </div>
       </form>
       <NewCoffeeShop handleAddShop={props.handleAddShop} />
-        <div className='shops-in-club'>
+        <div className={styles['shops-in-club']}>
           <p>Here's the coffee shops in you area! Select a shop to host your club in and let others join!</p>
-          <div className='shop-cards'>
+          <div className={styles['shop-cards']}>
             <div>Shop Card 1</div>
             <div>Shop Card 2</div>
             <div>Shop Card 3</div>
