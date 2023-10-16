cf5ceb6547249ab428353b5f24004bf7ce1f00cf
Nate Sobeck <nsobeck910@gmail.com>
2023-10-16 16:28:31 -0400

    add basic layout for coffeeShop index page

--- src/components/AllCoffeeShops/AllCoffeeShops.jsx
+++ src/components/AllCoffeeShops/AllCoffeeShops.jsx
@@ -7,6 +7,10 @@ import styles from "./AllCoffeeShops.module.css"
 const AllCoffeeShops = ({ shops }) => {
   return ( 
     <div className={styles["shops-container"]}>
+      <div className={styles.label}>
+        <h3>Name:</h3>
+        <h3>Location:</h3>
+      </div>
       {shops.map(shop => <ShopCard key={shop._id} shop={shop}/>)}
     </div>
   )
--- src/components/ShopCard/ShopCard.jsx
+++ src/components/ShopCard/ShopCard.jsx
@@ -7,7 +7,10 @@ import styles from './ShopCard.module.css'
 const ShopCard = ({ shop }) => {
   return (
     <Link to={`/coffeeshops/${shop._id}`}>
-      <h3>{shop.name}</h3>
+      <div className={styles["shop-card-container"]}>
+        <h3>{shop.name}</h3>
+        <div>{shop.location}, {shop.address.state}</div>
+      </div>
     </Link>
   )
 }
