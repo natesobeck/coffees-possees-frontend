d093589ab88477c338ddd2f156e548dd9c234224
Ellie <eagyeman1234@gmail.com>
2023-10-17 14:16:54 -0400

    display reviews on coffeshop details page

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -1,19 +1,20 @@
 import styles from './ReviewCard.module.css'
 
+//components 
+// import AddedBy from '../AddedBy/AddedBy'
+
 const ReviewCard = ({ review }) => {
   
 
   return (
-    // <div>
-    //   <h1> this is a review card</h1>
-    //   <AddedBy content={review} />
-    //   <h3>{review.text}</h3>
-    //   <h3>{review.price}</h3>
-    //   <h3>{review.rating}</h3>
-    //   <h3>{review.coffeeShopAmbience}</h3>
-    //   <h3>{review.wifi}</h3>
-    // </div>
-  
+    <div>
+      {/* <AddedBy content={review} /> */}
+      <h3>{review.text}</h3>
+      <h3>{review.price}</h3>
+      <h3>{review.rating}</h3>
+      <h3>{review.coffeeShopAmbience}</h3>
+      <h3>{review.wifi}</h3>
+    </div>
   )
 }
 
