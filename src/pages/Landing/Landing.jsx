9600d38590cb84169062be5e61f6474134745459
Nate Sobeck <nsobeck910@gmail.com>
2023-10-18 13:00:03 -0400

    refine styling for landing page and add background for about-us

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -11,17 +11,6 @@ const Landing = ({ user }) => {
       <div id={styles['home-background']}></div>
       <div id={styles['img-cover1']}></div>
       <section className={styles['section-container']} id={styles['home']}>
-        {/* <div className={styles['wooden-board']}>
-          <h1>Welcome, {user ? user.name.slice(0, 1).toUpperCase().concat(user.name.slice(1).toLowerCase()) : 'friend'}</h1>
-        </div>
-        <img src="/Logo.png" alt="Coffees and Posses Logo" />
-        <br />
-        <p>Do you love coffee?</p>
-        <br />
-        <p>Got some interests but don't have anybody to talk about them with? Make a club for it!</p>
-        <br />
-        <p>We'd love to join you too. That's what Coffees and Posses is for. Movies, Sports, Dancing, JetSkiing, you name it. There's a club for it.</p>
-        <h2>Bringing Like-Minded People Together</h2> */}
         <div id={styles['title-container']}>
           <div>Coffees and Posses</div>
         </div>
@@ -36,8 +25,9 @@ const Landing = ({ user }) => {
       <section className={styles['section-container']} id={styles['reccomendations']}>
         <h1>This is where the club reccomendations go.</h1>
       </section>
+      <div id={styles['about-background']}></div>
+      <div id={styles['img-cover3']}></div>
       <section className={styles['section-container']} id={styles['about-us']}>
-        <h1>This is out About Us section.</h1>
         <AboutUs />
       </section>
     </main>
