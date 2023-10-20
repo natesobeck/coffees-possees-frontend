5979304d45c11d3af022c245e83f1a50344dfbb7
Tuan Nguyen <tuan.nat915@gmail.com>
2023-10-20 09:42:43 -0400

    Update aboutUs infomation

--- src/data/aboutUs.js
+++ src/data/aboutUs.js
@@ -1,26 +1,27 @@
 export const data = [
   {
     name: 'Ellie Agy',
-    github: '#',
-    linkedIn: '#',
-    personal_brand: ''
+    github: 'https://github.com/Ellieellie12',
+    linkedIn: 'https://www.linkedin.com/in/eagyeman/',
+    personal_brand: 'Along her journey of pursuing her dreams of becoming a famous singer a new found love for coding opened up. Mixing her creative ideas with problem solving skills has worked hand in hand.'
   },
   {
-    name: 'Nate SoBeck',
-    github: '#',
-    linkedIn: '#',
-    personal_brand: ''
+    name: 'Nate Sobeck',
+    github: 'https://github.com/natesobeck',
+    linkedIn: 'https://www.linkedin.com/in/n-sobeck/',
+    personal_brand: "After spending years pursiung passions in helping others achieve personal goals through personal training, prison reentry and more, Nate shifted his focus to a new passion: software development. He uses his natural problem solving skills to create sleek, user-friendly and reliable and robust applications. Come check out his work!"
   },
   {
     name: 'Jonathan Takam',
-    github: '#',
-    linkedIn: '#',
-    personal_brand: ''
+    github: 'https://github.com/Johny270',
+    linkedIn: 'https://www.linkedin.com/in/takam-jonathan/',
+    personal_brand: "A fellow developer, sometimes crazy, but not that much though. You might catch me watching anime sometimes too, please, don't judge me! Loves self-expression and so often draws to express his different emotional states."
   },
   {
     name: 'Tuan Nguyen',
-    github: '#',
-    linkedIn: '#',
-    personal_brand: ''
+    github: 'https://github.com/TuanNguyen0915',
+    linkedIn: 'https://www.linkedin.com/in/tuan-nguyen0915/',
+    personal_brand: "I'm a full-stack software engineer who enjoys building apps for accessible and intuitive design to give the customer the best and simplest way solutions. I am interested in technology and techniques in the full-stack development field to keep my passion for it."
+
   },
 ]
